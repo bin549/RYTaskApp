ed42938f42c1af9745016b6cffabfe07ae0dfd3d
bin549 <549288160@qq.com>
2023-06-11 13:27:29 +0800

    番茄闹钟 Done

--- RYTaskApp/PomodoroViewController.swift
+++ RYTaskApp/PomodoroViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class PomodoroViewController: UIViewController {
+class PomodoroViewController: UIViewController, CAAnimationDelegate {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
@@ -8,13 +8,10 @@ class PomodoroViewController: UIViewController {
     let backProgressLayer = CAShapeLayer()
     let animation = CABasicAnimation(keyPath: "strokeEnd")
 
-
     var timer = Timer()
     var isTimerStarted = false
     var isAnimationStarted = false
-    // var time = 1500
-    var time = 5
-
+    var time = 1500
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,22 +28,27 @@ class PomodoroViewController: UIViewController {
         cancelButton.isEnabled = true
         cancelButton.alpha = 1.0
         if !isTimerStarted {
+            drawForeLayer()
+            startResumeAnimation()
             startTimer()
             isTimerStarted = true
-            startButton.setTitle("Pause", for: .normal)
+            startButton.setTitle("暂停", for: .normal)
             startButton.setTitleColor(UIColor.orange, for: .normal)
         } else {
+            pauseAnimation()
             timer.invalidate()
             isTimerStarted = false
-            startButton.setTitle("Resume", for: .normal)
+            startButton.setTitle("继续", for: .normal)
             startButton.setTitleColor(UIColor.green, for: .normal)
         }
     }
 
     @IBAction func cancelButtonTapped(_ sender: Any) {
+        stopAnimation()
         cancelButton.isEnabled = false
         cancelButton.alpha = 0.5
-        startButton.setTitle("Start", for: .normal)
+        startButton.setTitle("开始", for: .normal)
+        startButton.setTitleColor(UIColor.green, for: .normal)
         timer.invalidate()
         time = 1500
         isTimerStarted = false
@@ -61,10 +63,10 @@ class PomodoroViewController: UIViewController {
         if time<1 {
             cancelButton.isEnabled = false
             cancelButton.alpha = 0.5
-            startButton.setTitle("Start", for: .normal)
+            startButton.setTitle("开始", for: .normal)
             startButton.setTitleColor(UIColor.green, for: .normal)
             timer.invalidate()
-            time = 5
+            time = 1500
             isTimerStarted = false
             timeLabel.text = "25:00"
         } else {
@@ -86,12 +88,78 @@ class PomodoroViewController: UIViewController {
         backProgressLayer.lineWidth = 15
         view.layer.addSublayer(backProgressLayer)
     }
+
+    func drawForeLayer() {
+        foreProgressLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY), radius: 100, startAngle: -90.degressToRadians, endAngle: 270.degressToRadians, clockwise: true).cgPath
+        foreProgressLayer.strokeColor = UIColor.red.cgColor
+        foreProgressLayer.fillColor = UIColor.clear.cgColor
+        foreProgressLayer.lineWidth = 15
+        view.layer.addSublayer(foreProgressLayer)
+    }
+    
+    func startResumeAnimation() {
+        if !isAnimationStarted {
+            startAnimation()
+        } else {
+            resumeAnimation()
+        }
+    }
+
+    func startAnimation() {
+        resetAnimation()
+        foreProgressLayer.strokeEnd = 0.0
+        animation.keyPath = "strokeEnd"
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = 1500
+        animation.delegate = self
+        animation.isRemovedOnCompletion = false
+        animation.isAdditive = true
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        foreProgressLayer.add(animation, forKey: "strokeEnd")
+        isAnimationStarted = true
+    }
+
+    func resetAnimation() {
+        foreProgressLayer.speed = 1.0
+        foreProgressLayer.timeOffset = 0.0
+        foreProgressLayer.beginTime = 0.0
+        isAnimationStarted = false
+    }
+    
+    func pauseAnimation() {
+        let pausedTime = foreProgressLayer.convertTime(CACurrentMediaTime(), from: nil)
+        foreProgressLayer.speed = 0.0
+        foreProgressLayer.timeOffset = pausedTime
+    }
+    
+    func resumeAnimation() {
+        let pausedTime = foreProgressLayer.timeOffset
+        foreProgressLayer.speed = 1.0
+        foreProgressLayer.timeOffset = 0.0
+        foreProgressLayer.beginTime = 0.0
+        let timeSincePaused = foreProgressLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        foreProgressLayer.beginTime = timeSincePaused
+    }
+    
+    func stopAnimation() {
+        foreProgressLayer.speed = 1.0
+        foreProgressLayer.timeOffset = 0.0
+        foreProgressLayer.beginTime = 0.0
+        foreProgressLayer.strokeEnd = 0.0
+        foreProgressLayer.removeAllAnimations()
+        isAnimationStarted = false
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        stopAnimation()
+    }
 }
 
 
 extension Int {
     var degressToRadians: CGFloat {
-        return CGFloat(self) * .pi / 100
+        return CGFloat(self) * .pi / 180
     }
 }
     
